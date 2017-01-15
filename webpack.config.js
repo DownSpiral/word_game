99e8479be5dba2cd9d4ee7dd38aad459da6cb69b
DownSpiral <eric.d.wishart@gmail.com>
2017-01-14 19:42:56 -0800

    Add snow background effect

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'public/js');
+var IMAGES = path.resolve(__dirname, 'public/images');
 var APP_DIR = path.resolve(__dirname, 'private/react');
 
 var config = {
@@ -20,7 +21,13 @@ var config = {
         test: /\.scss$/,
         include: APP_DIR,
         loaders: ['style', 'css', 'sass']
-      }
+      }, {
+        test: /\.png$/,
+        use: { loader: 'url-loader', options: { limit: 100000 } },
+      }, {
+        test: /\.(jpe?g|gif|png)$/,
+        loader: 'file-loader?emitFile=false&name=[path][name].[ext]'
+      },
     ]
   }
 };
